63056ea6e96a19c6f56daafbc217f155140840e7
Andreas Streichardt <andreas@mop.koeln>
2023-01-27 19:56:06 +0100

    ✨ change bezahlworkflow

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,36 +1,74 @@
 <template>
-  <el-form ref="form" :model="form" label-width="120px">
-    <el-form-item label="Bezahlen als">
-      <UserList v-model="form.selectedUser" />
-    </el-form-item>
-    <el-form-item>
-      <el-button
-        type="primary"
-        :disabled="form.selectedUser == ''"
-        @click="checkout"
+  <el-dialog title="Nutzer:in wählen" v-model="dialogVisible" width="95%">
+    <div v-if="loading" v-loading="loading">Loading...</div>
+    <div v-if="!loading">
+      <el-row type="flex" :gutter="20">
+        <el-col :span="24"
+          ><el-input placeholder="Nutzer:in suchen" v-model="search"></el-input
+        ></el-col>
+      </el-row>
+      <el-row type="flex" :gutter="20">
+        <el-col :span="4" v-for="user in filteredUsers">
+          <el-card class="card" @click="checkout(user.id)">
+            <el-row>
+              <el-col><img :src="gravatar(user.email)" class="image" /></el-col>
+            </el-row>
+            <el-row>
+              <el-col>{{ user.name }}</el-col>
+            </el-row>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+  </el-dialog>
+  <el-row>
+    <el-col :span="24"
+      ><el-button type="primary" @click="openUserDialog()"
         >Bezahlen</el-button
-      >
-    </el-form-item>
-  </el-form>
+      ></el-col
+    >
+  </el-row>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 import UserList from "./UserList.vue";
+import MD5 from "crypto-js/md5";
 
 export default defineComponent({
   components: { UserList },
   name: "Checkout",
   data() {
     return {
+      dialogVisible: false,
       form: { selectedUser: "" },
+      loading: false,
+      users: [],
+      search: "",
     };
   },
   methods: {
-    checkout() {
-      this.$emit("checkout", this.form.selectedUser);
-      this.form.selectedUser = "";
+    gravatar(email: string) {
+      return `https://www.gravatar.com/avatar/${MD5(email)}`;
+    },
+    async openUserDialog() {
+      this.users = [];
+      this.dialogVisible = true;
+      this.loading = true;
+      let response;
+      try {
+        response = await fetch("/mete/api/v1/users.json");
+        this.users = await response.json();
+      } catch (e) {
+        this.loading = false;
+        throw e;
+      }
+      this.loading = false;
+    },
+    checkout(userId: string) {
+      this.dialogVisible = false;
+      this.$emit("checkout", userId);
     },
   },
   props: {
@@ -39,5 +77,18 @@ export default defineComponent({
     },
   },
   emits: ["checkout"],
+  computed: {
+    filteredUsers: function () {
+      return this.users.filter((user) => {
+        return user.name.toLowerCase().includes(this.search.toLowerCase());
+      });
+    },
+  },
 });
 </script>
+
+<style scoped>
+.card {
+  cursor: pointer;
+}
+</style>
