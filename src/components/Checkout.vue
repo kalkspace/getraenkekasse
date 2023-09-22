0d10383fece850778b3860b94e69fbd6926e2692
Andreas Streichardt <andreas@mop.koeln>
2023-09-22 17:29:43 +0200

    ✨ add last users

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -3,13 +3,32 @@
     <div v-if="loading" v-loading="loading">Loading...</div>
     <div v-if="!loading">
       <el-row type="flex" :gutter="20">
+        <el-col>
+          <h3>Letzte Nutzer:innen</h3>
+        </el-col>
+      </el-row>
+      <el-row type="flex" :gutter="20">
+        <!-- todo refactor into UserCard component -->
+        <el-col :span="4" v-for="user in lastUsers">
+          <el-card class="card" @click="checkout(user)">
+            <el-row>
+              <el-col><img :src="gravatar(user.email)" class="image" /></el-col>
+            </el-row>
+            <el-row>
+              <el-col>{{ user.name }}</el-col>
+            </el-row>
+          </el-card>
+        </el-col>
+      </el-row>
+      <el-divider />
+      <el-row type="flex" :gutter="20">
         <el-col :span="24"
           ><el-input placeholder="Nutzer:in suchen" v-model="search"></el-input
         ></el-col>
       </el-row>
       <el-row type="flex" :gutter="20">
         <el-col :span="4" v-for="user in filteredUsers">
-          <el-card class="card" @click="checkout(user.id)">
+          <el-card class="card" @click="checkout(user)">
             <el-row>
               <el-col><img :src="gravatar(user.email)" class="image" /></el-col>
             </el-row>
@@ -33,15 +52,17 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-import UserList from "./UserList.vue";
 import MD5 from "crypto-js/md5";
 import { User } from "../types/mete";
 
 export default defineComponent({
-  components: { UserList },
   name: "Checkout",
+  mounted() {
+    this.updateLastUsers();
+  },
   data() {
     return {
+      lastUsers: [] as User[],
       dialogVisible: false,
       form: { selectedUser: "" },
       loading: false,
@@ -54,6 +75,7 @@ export default defineComponent({
       return `https://www.gravatar.com/avatar/${MD5(email)}`;
     },
     async openUserDialog() {
+      this.updateLastUsers();
       this.users = [];
       this.dialogVisible = true;
       this.loading = true;
@@ -67,9 +89,36 @@ export default defineComponent({
       }
       this.loading = false;
     },
-    checkout(userId: number) {
+    updateLastUsers() {
+      this.lastUsers = [];
+      const storedLastUsers = localStorage.getItem("lastUsers");
+      if (storedLastUsers !== null) {
+        try {
+          const value = JSON.parse(storedLastUsers) as User[];
+          // enough check for now
+          if (Array.isArray(value)) {
+            this.lastUsers = value;
+          }
+        } catch (e) {
+          console.error(`couldn't parse lastUsers: ${e}`);
+        }
+      }
+    },
+    checkout(user: User) {
+      localStorage.setItem(
+        "lastUsers",
+        // remove new user from existing list if present, put user to the front and limit list to 6 entries
+        JSON.stringify(
+          [
+            user,
+            ...this.lastUsers.filter(
+              (existingUser) => existingUser.id !== user.id
+            ),
+          ].slice(0, 6)
+        )
+      );
       this.dialogVisible = false;
-      this.$emit("checkout", userId);
+      this.$emit("checkout", user.id);
     },
   },
   props: {
